ae38adb0b35bbae4200ef1089f7a36847f38f1bf
rekhamanasa <bhumireddyrekhamanasa@gmail.com>
2020-03-04 21:24:56 +0530

    Custom Collections

---
+++ task6/src/main/java/com/mrecw/task6/App.java
@@ -0,0 +1,14 @@
+package com.mrecw.task6;
+
+public class App extends Customcoll
+{
+	
+    public static void main( String[] args )
+    {
+    	App ob=new App();
+    	ob.select();
+    }
+}
+
+
+
